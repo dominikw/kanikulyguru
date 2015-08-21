f29bc373c13edfcc467fb46127450cded594f279
Adam Nowak <adam.nowak@netguru.co>
2015-08-21 18:17:05 +0200

    fix algorithm

--- lib/holiday_calculator.rb
+++ lib/holiday_calculator.rb
@@ -2,12 +2,12 @@ require 'toggl_api'
 
 class HolidayCalculator
   VACATION_PROJECTS = [3561207]
-  NOT_WORK_PROJECTS = [3561198] + VACATION_PROJECTS
+  NOT_WORK_PROJECTS = VACATION_PROJECTS
 
   def available_holiday(toggl_token, dg_start)
-    entries = fetch_entries(toggl_token, dg_start)
-    work_entries = entries.reject { |entry| NOT_WORK_PROJECTS.include?(entry.pid) }
+    entries = fetch_entries(toggl_token, DateTime.parse(dg_start))
     vacation_entries = entries.select { |entry| VACATION_PROJECTS.include?(entry.pid) }
+    work_entries = entries.reject { |entry| NOT_WORK_PROJECTS.include?(entry.pid) }
     difference = sum_duration(work_entries) / 16.0 - sum_duration(vacation_entries)
     {
       hours_left: to_hours(difference),
@@ -21,7 +21,14 @@ class HolidayCalculator
 
   def fetch_entries(toggl_token, dg_start)
     base = Toggl::Base.new toggl_token
-    entries = base.time_entries(dg_start, Time.now)
+    entries = []
+    i_date = dg_start
+    while i_date < DateTime.now
+      next_date = [i_date + 150, DateTime.now].min
+      entries += base.time_entries(i_date, next_date)
+      i_date = next_date + 1
+    end
+    entries
   end
 
   def sum_duration(entries)
